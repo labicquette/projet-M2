a4265da29318c217ffdb2227d88f3175a8ce6d00
labicquette <komete17@gmail.com>
2025-01-08 23:31:27 +0100

    modif eval et fix utils

--- eval.py
+++ eval.py
@@ -17,7 +17,6 @@ examples_dict = {
 
 def evaluation():
     rouge = evaluate.load("rouge")
-    sari = Sari()
     models = dirs  = [ f.path for f in os.scandir("./runs/Summary") if f.is_dir() ]
     for m in models:
         dirs  = [ f.path for f in os.scandir(m) if f.is_dir() ]
@@ -26,14 +25,16 @@ def evaluation():
             name = d.split("/")[-1]
             model = d_splits[-2]
             ds = d_splits[-1]
+            #print(model, ds)
             splits  = [ f.path for f in os.scandir(d) if f.is_dir() ]
             for split in splits :
+                print(split)
                 if "cnn" in split:
+                    continue
                     dataset = load_dataset("abisee/cnn_dailymail","3.0.0",trust_remote_code=True) 
                 else:
                     dataset = load_dataset(dataset_paths[name],trust_remote_code=True)
                 files = glob(split + "/*")
-                #for f in files :
                 column = []
                 for row in dataset[split.split("/")[-1]] :
                     with open(split+"/"+get_file(row, name), "r")as f:
@@ -41,10 +42,15 @@ def evaluation():
                 dataset[split.split("/")[-1]].add_column("inferred", column)
                 sources = dataset[split.split("/")[-1]][examples_dict[name]]
                 references = dataset[split.split("/")[-1]][labels_dict[name]]
+                
                 result = rouge.compute(predictions=column, references=references, use_stemmer=True)
-                sari_results = sari.compute(sources=[sources], predictions=[column], references=[references])
-                print("ROUGE : ", result)
-                #print("SARI : ", sari_results.keys())
+                print("ROUGE RESULTS : ", result)
+                res = "\n" + model + ds + split.split("/")[-1] + str(result["rouge1"]) + "," + str(result["rouge2"]) +  "," + str(result["rougeL"]) +  "," + str(result["rougeLsum"])
+                
+                with open("./results.csv", "a") as file:
+                    file.write(res)
+    
+
     
     print("break")
 
@@ -52,4 +58,6 @@ def evaluation():
     return 0
 
 if __name__ == '__main__':
+    with open("./results.csv", "w") as file:
+        file.write("model,dataset,run,rouge1,rouge2,rougeL,rougeLsum")
     evaluation()
--- source/utils.py
+++ source/utils.py
@@ -7,7 +7,7 @@ labels_dict = {"cnn_dailymail":"highlights",
 def get_file(dataset, name):
     if name == "SCOTUS":
         id_case = dataset["justia_link"].split("/")[-3:-1]
-        file = dataset["case_name"].replace("/","")+ "-"+ id_case[0]+ "-"+ id_case[1]
+        file = dataset["case_name"].replace("/","").replace("'","")+ "-"+ id_case[0]+ "-"+ id_case[1]
 
     if name == "cnn_dailymail":
         file = dataset["id"] # to set 
