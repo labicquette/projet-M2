4e6a31b7c3e71876e12f8c36dec062235b8eda91
yaboiiii23 <hreshviksewraj@gmail.com>
2025-01-09 19:58:49 +0000

    save

--- source/inference/llama_small_sum.py
+++ source/inference/llama_small_sum.py
@@ -45,24 +45,25 @@ def inference(dataset, **parameters):
     examples = parameters["examples"]
     print(dataset)
     # Ensure save_path directory exists
-    save_path = "/content/drive/MyDrive/projet_m2/data/small_sum/"
+    save_path = "/content/drive/MyDrive/projet_m2/data/small_sum_2/"
     os.makedirs(save_path, exist_ok=True)
 
-    for i, ex in enumerate(tqdm(dataset["train"][examples])):
-    #dataset["train"].map(lambda ex,i : mapped(ex, client, save_path, dataset, parameters, run,i), with_indices=True)
-      ex_no_html = remove_html_tags(ex)
-      response = client.chat(model='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF:Q4_K_M', 
-      options={"num_ctx": 128000, "num_gpu":60},
-      messages=[
-          {
-              'role': 'user',
-              'content': ("You are an expert at writing short summaries. Your job is to summarize the text below in exactly " +
-                          "3 SHORT SENTENCES. Focus only on the most important information and leave out everything extra in order " +
-                          "to make 3 SHORT SENTENCES. Here's the text: " + ex_no_html +
-                          "make sure to make 3 SHORT SENTENCES.Not more"),
-          },
-      ])
-      file = get_file(dataset[run][i], parameters["dataset_name"])
-      with open(save_path + file, "w") as f:
-          f.write(response['message']['content'])
+    for i, ex in enumerate(tqdm(dataset["train"][examples][67:])):
+
+          #dataset["train"].map(lambda ex,i : mapped(ex, client, save_path, dataset, parameters, run,i), with_indices=True)
+            ex_no_html = remove_html_tags(ex)
+            response = client.chat(model='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF:Q4_K_M', 
+            options={"num_ctx": 128000, "num_gpu":60},
+            messages=[
+                {
+                    'role': 'user',
+                    'content': ("You are an expert at writing short summaries. Your job is to summarize the text below in exactly " +
+                                "3 SHORT SENTENCES. Focus only on the most important information and leave out everything extra in order " +
+                                "to make 3 SHORT SENTENCES. Here's the text: " + ex_no_html +
+                                "make sure to make 3 SHORT SENTENCES.Not more"),
+                },
+            ])
+            file = get_file(dataset[run][i], parameters["dataset_name"])
+            with open(save_path + file, "w") as f:
+                f.write(response['message']['content'])
   
