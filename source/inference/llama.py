4cba03d24ccd52c2b1db1d002aa1166b1c6bae8d
Théo Charlot <62436507+labicquette@users.noreply.github.com>
2025-01-09 16:26:04 +0100

    Update llama.py

--- source/inference/llama.py
+++ source/inference/llama.py
@@ -18,12 +18,13 @@ def inference(dataset, **parameters):
     examples = parameters["examples"]
 
     for i,ex in enumerate(tqdm(dataset[run][examples])):
-        response = client.chat(model='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF:IQ3_M', messages=[
+        response = client.chat(model='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF:Q4_K_M', messages=[
                 {
                     'role': 'user',
                     'content': "Resume the content of the following text while keeping all the information" + ex,
                 },
-                ])
+                ],
+                options={"num_ctx":128000, "num_gpu":60})
         print(response['message']['content'])
         file = get_file(dataset[run][i], parameters["dataset_name"])
         with open(parameters["save_path"]+file, "w") as f:
