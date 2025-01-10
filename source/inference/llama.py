5d92db77dda53d64548b1d03660f23e74e144110
labicquette <komete17@gmail.com>
2025-01-10 11:19:11 +0100

    attn_implementation

--- source/inference/llama.py
+++ source/inference/llama.py
@@ -37,7 +37,7 @@ def inference(dataset, **parameters):
 
     examples = parameters["examples"]
     model_id = "meta-llama/Llama-3.2-1B"
-    model = AutoModelForCausalLM.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id, attn_implementation = 'flash_attention_2')
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     pipe = pipeline("text-generation",
                     model=model,
